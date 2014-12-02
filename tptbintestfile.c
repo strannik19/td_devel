ac59aec1fc33cdd0dce3caf010366ebdb1d9dcd4
Andreas Wenzel <andreas.wenzel@teradata.com>
2014-12-02 19:53:54 +0100

    Added argument for filename

--- tptbintestfile.c
+++ tptbintestfile.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv) {
 		return(2);
 	}
 
-	ptr_myfile=fopen("test.bin","a");
+	ptr_myfile=fopen(argv[optind],"a");
 	
 	if (!ptr_myfile) {
 		printf("Unable to open file!");
