9c0df780bacc1feac3fe68412e08c5a67e4a9949
Andreas Wenzel <andreas.wenzel@teradata.com>
2014-12-06 18:36:23 +0100

    Added support for indicator bits

--- tools/tptbin/tptbintestfile.c
+++ tools/tptbin/tptbintestfile.c
@@ -15,8 +15,10 @@ int main(int argc, char **argv) {
 	double calcrowlen = 0;
 	int c;
 	int maxlen = 40;
+	char indicator = 0;
+	int numindic = 0;
 
-	while ((c = getopt (argc, argv, "c:r:m:h")) != -1) {
+	while ((c = getopt (argc, argv, "c:r:m:hi")) != -1) {
 		switch (c) {
 			case 'c':
 				numcols = atoi(optarg);
@@ -28,8 +30,11 @@ int main(int argc, char **argv) {
 				maxlen = atoi(optarg);
 				break;
 			case 'h':
-				printf("usage: %s [-c numcols] [-r numrows] [-m maxcollen]\n", argv[0]);
+				printf("usage: %s [-c numcols] [-r numrows] [-m maxcollen] [-i]\n", argv[0]);
 				return(1);
+				break;
+			case 'i':
+				indicator = 1;
 				break;
 			case '?':
 				if (optopt == 'c' || optopt == 'r' || optopt == 'm')
@@ -42,28 +47,27 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	int numindic;
-	if (numcols % 8 == 0) {
-		numindic = numcols / 8;
-	} else {
-		numindic = numcols / 8 + 1;
-	}
-	unsigned char indic[numindic];
-	for (i = 0; i < numindic; i++) {
-		indic[i] = 0x00;
-	}
-
+	unsigned short collen[numcols];
 	char *col[numcols];
-	unsigned short collen[numindic];
+	numindic = (numcols + 7) / 8;    // calculate number of indicator bytes base on number of columns
 
+	unsigned char indic[numindic];
+
+	if (indicator == 1) {
+		for (i = 0; i < numindic; i++) {
+			indic[i] = 0x00;
+		}
+
+		rowlen += numindic;
+	}
+
+	// Set pointer for random data to write
 	for (j = 0; j < numcols; j++) {
 		col[j] = &text[j];
 		collen[j] = rand() % maxlen;
 		rowlen += collen[j] + 2;
 		calcrowlen += collen[j] + 2;
 	}
-
-	rowlen += numindic;
 
 	if (calcrowlen + numindic > 32768) {
 		fprintf(stderr, "Rowlen got too long: %.0f!\n", calcrowlen + numindic);
@@ -80,7 +84,9 @@ int main(int argc, char **argv) {
 
 	for (j = 0; j < numrows; j++) {
 		fwrite(&rowlen, sizeof(rowlen), 1, ptr_myfile); // write row len
-		fwrite(&indic, sizeof(indic), 1, ptr_myfile); // write indicator bytes
+		if (indicator == 1) {
+			fwrite(&indic, sizeof(indic), 1, ptr_myfile); // write indicator bytes
+		}
 		for (i = 0; i < numcols; i++) {
 			fwrite(&collen[i], sizeof(collen[i]), 1, ptr_myfile);
 			fwrite(text, collen[i], 1, ptr_myfile);
