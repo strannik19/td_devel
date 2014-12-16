8e57e84be75c68156efa36b4ed6b7dce641beb06
Andreas Wenzel <andreas.wenzel@teradata.com>
2014-12-16 11:33:20 +0100

    Extended Standards.h

--- tools/tptbin/Standards.h
+++ tools/tptbin/Standards.h
@@ -2,8 +2,11 @@
 #define STANDARDS_H
 
 #define MAXCOLS 10000
-#define MAXBUF 1000000
+#define MAXBUF 80000
 #define TRUE 0
 #define FALSE 1
+#define MAXLENDELIM 10
+#define MAXLENQUOTE 10
+#define MAXNUMSELROWS 10000
 
 #endif
