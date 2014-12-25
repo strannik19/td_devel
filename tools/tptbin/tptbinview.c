b15594691a37b2acd87d38e9176c91ef63c11e9a
Andreas Wenzel <andreas.wenzel@teradata.com>
2014-12-25 15:23:12 +0100

    License update

--- tools/tptbin/tptbinview.c
+++ tools/tptbin/tptbinview.c
@@ -2,8 +2,6 @@
 	Copyright (c) 2014 Andreas Wenzel, Teradata Germany
 	License: You are free to use, adopt and modify this program for your particular
 	purpose.
-	If you are a Teradata employee you are free to use, copy, and distribute
-	this program to Teradata customers.
 	If you don't have any relationship with Teradata, you will find this tool
 	probably not very useful.
 	LICENSOR IS NOT LIABLE TO LICENSEE FOR ANY DAMAGES, INCLUDING COMPENSATORY,
