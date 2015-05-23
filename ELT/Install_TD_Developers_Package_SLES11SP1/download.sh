b0569ed9c1f91d429bb837fe4a7ee40c098d2d72
Andreas Wenzel <andreas.wenzel@teradata.com>
2015-05-23 21:33:48 +0200

    Updated to newer releases

--- ELT/Install_TD_Developers_Package_SLES11SP1/download.sh
+++ ELT/Install_TD_Developers_Package_SLES11SP1/download.sh
@@ -7,21 +7,26 @@
 ERRORPACKAGES=""
 
 for DL in \
-	http://artfiles.org/apache.org/apr/apr-1.5.1.tar.bz2 \
-	http://mirror.dkd.de/apache//apr/apr-1.5.1.tar.bz2 \
+	http://ftp.fau.de/apache/apr/apr-1.5.2.tar.bz2 \
+	http://ftp.fau.de/apache/apr/apr-util-1.5.4.tar.bz2 \
 	http://curl.haxx.se/download/curl-7.40.0.tar.bz2 \
-	https://cpan.metacpan.org/authors/id/M/MJ/MJEVANS/DBD-ODBC-1.50.tar.gz \
+	https://cpan.metacpan.org/authors/id/M/MJ/MJEVANS/DBD-ODBC-1.52.tar.gz \
 	https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.633.tar.gz \
 	https://cpan.metacpan.org/authors/id/B/BI/BINGOS/ExtUtils-MakeMaker-7.04.tar.gz \
-	https://www.kernel.org/pub/software/scm/git/git-2.2.2.tar.gz \
-	https://www.openssl.org/source/openssl-1.0.2.tar.gz \
+	https://www.kernel.org/pub/software/scm/git/git-2.4.1.tar.gz \
+	https://www.openssl.org/source/openssl-1.0.2a.tar.gz \
 	http://prdownloads.sourceforge.net/scons/scons-2.3.4.tar.gz \
 	http://serf.googlecode.com/svn/src_releases/serf-1.3.8.tar.bz2 \
 	http://www.sqlite.org/2015/sqlite-amalgamation-3080801.zip \
-	http://mirror.arcor-online.net/www.apache.org/subversion/subversion-1.8.11.tar.bz2 \
+	http://mirror.synyx.de/apache/subversion/subversion-1.8.13.tar.bz2 \
 	http://search.cpan.org/CPAN/authors/id/E/EX/EXODIST/Test-Simple-1.001014.tar.gz
 do
-	wget "${DL}" -O "${DL##*/}"
+	if [ "${DL#https://cpan.metacpan.org}" = "${DL}" ]
+	then
+		wget "${DL}" -O "${DL##*/}"
+	else
+		wget --no-check-certificate "${DL}" -O "${DL##*/}"
+	fi
 
 	if [ $? -ne 0 ]
 	then
