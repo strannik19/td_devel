fd5ed44dd50bc2d55164d343615c444aa33cc578
Andreas Wenzel <andreas.wenzel@teradata.com>
2015-05-30 15:09:45 +0200

    Fixed indenting

--- tools/tptbin/python/tptbin.py
+++ tools/tptbin/python/tptbin.py
@@ -22,64 +22,63 @@ import logging
 
 def readrowlen(f):
      try:
-         a = f.read(2)
+          a = f.read(2)
      except:
-         return (-1)
+          return (-1)
  
      if len(a) == 2:
-         rowlen = struct.unpack('H', a)[0]
+          rowlen = struct.unpack('H', a)[0]
      else:
-         return (-1)
+          return (-1)
 
      return (rowlen)
 
 
 def readrow(f, rowlen, numrow):
-    completerow = f.read(rowlen)
+     completerow = f.read(rowlen)
 
-    if (len(completerow) != rowlen):
-        logging.warning("File: %s: Record: %d: Error in len!",
-                        filename, numrow)
-        return (False)
+     if (len(completerow) != rowlen):
+          logging.warning("File: %s: Record: %d: Error in len!",
+                          filename, numrow)
+          return (False)
 
-    return (completerow)
+     return (completerow)
 
 
 def numcolumns(filename, record, indicator, rownum):
-    recordlen = len(record)
-    columns = 0
+     recordlen = len(record)
+     columns = 0
 
-    if indicator == 1:
-        # data has indicator
-        pass
+     if indicator == 1:
+          pass
 
-    elif indicator == 2:
-        # data has no indicator
-        pos = 0
+     elif indicator == 2:
+          # data has no indicator
+          pos = 0
 
-        while pos < recordlen:
-            # still not at the end of the record
-            columnsize = struct.unpack('H', record[pos:pos + 2])[0]
+          while pos < recordlen:
+               # still not at the end of the record
+               columnsize = struct.unpack('H', record[pos:pos + 2])[0]
 
-            pos += 2
+               pos += 2
 
-            column = record[pos:pos + columnsize]
+               column = record[pos:pos + columnsize]
 
-            if (len(column) == columnsize):
-                # column is as long as expected
-                columns += 1
-                pos += columnsize
-            elif len(column) > 0 and columnsize > 0:
-                # error (columnsize bigger zero but not as big as expected)
-                logging.warning("File: %s: Error in row %d, column %d. "
-                                "Found %d bytes instead of %d!", filename,
-                                rownum, columns, len(column), columnsize)
-            else:
-                break
+               if (len(column) == columnsize):
+                    # column is as long as expected
+                    columns += 1
+                    pos += columnsize
+               elif len(column) > 0 and columnsize > 0:
+                    # error (columnsize bigger zero but not as big as expected)
+                    logging.warning("File: %s: Error in row %d, column %d. "
+                                    "Found %d bytes instead of %d!", filename,
+                                    rownum, columns, len(column), columnsize)
+               else:
+                    break
 
-    else:
-        # no indicator information given (indicator is unknown)
-        # trying to find out
-        pass
+     else:
+          # no indicator information given (indicator is unknown)
+          # trying to find out
+          pass
 
-    return (columns)
+     return (columns)
