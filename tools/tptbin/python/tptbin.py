8e3689c46e9900ebded5504648a415082e2a15c3
Andreas Wenzel <andreas.wenzel@teradata.com>
2015-06-24 08:11:55 +0200

    Fixed wrong indenting
    Komodo Edit has a funny behavior with indenting.
    Just with one file, Komodo uses 5 spaces instead of 4. And I don't find
    how to change it. Yes, in the Preferences it's ok.

--- tools/tptbin/python/tptbin.py
+++ tools/tptbin/python/tptbin.py
@@ -21,152 +21,152 @@ import sys
 import logging
 
 def readrowlen (f):
-     try:
-          a = f.read(2)
-     except:
-          return (-1)
+    try:
+        a = f.read(2)
+    except:
+        return (-1)
  
-     if len(a) == 2:
-          rowlen = struct.unpack('H', a)[0]
-     else:
-          return (-1)
+    if len(a) == 2:
+        rowlen = struct.unpack('H', a)[0]
+    else:
+        return (-1)
 
-     return (rowlen)
+    return (rowlen)
 
 
 def readrow (f, rowlen, numrow):
-     completerow = f.read(rowlen)
+    completerow = f.read(rowlen)
 
-     if (len(completerow) != rowlen):
-          logging.warning("File: %s: Record: %d: Error in len!",
-                          filename, numrow)
-          return (False)
+    if (len(completerow) != rowlen):
+        logging.warning("File: %s: Record: %d: Error in len!",
+                     filename, numrow)
+        return (False)
 
-     return (completerow)
+    return (completerow)
 
 
 def checkindicatorifcolumnisnull (indicators, pos):
-     return indicators & 2**pos != 0
+    return indicators & 2**pos != 0
 
 
 def numcolumns (filename, record, indicator, rownum, source):
-     recordlen = len(record)
-     columns = 0
-          
-     if indicator == 1:
-          # data has indicator (at least one ... start with second position)
-          indicatorpos = 1
+    recordlen = len(record)
+    columns = 0
+        
+    if indicator == 1:
+        # data has indicator (at least one ... start with second position)
+        indicatorpos = 1
 
-          while True:
-               # find number of indicator bytes
+        while True:
+            # find number of indicator bytes
 
-               if len(record[indicatorpos:indicatorpos + 2]) == 2:
-                    columnsize = struct.unpack('H',
-                                               record[indicatorpos:indicatorpos + 2])[0]
-               else:
-                    columns = False
-                    break
+            if len(record[indicatorpos:indicatorpos + 2]) == 2:
+                columnsize = struct.unpack('H',
+                                      record[indicatorpos:indicatorpos + 2])[0]
+            else:
+                columns = False
+                break
 
-               if columnsize > recordlen - indicatorpos - 2:
-                    # columnsize is bigger than remaining record
-                    # this is definitely still an indicator byte on indicatorpos
-                    indicatorpos += 1
-                    continue
+            if columnsize > recordlen - indicatorpos - 2:
+                # columnsize is bigger than remaining record
+                # this is definitely still an indicator byte on indicatorpos
+                indicatorpos += 1
+                continue
 
-               # Start point for possible columns
-               pos = indicatorpos
-               columns = 0
+            # Start point for possible columns
+            pos = indicatorpos
+            columns = 0
 
-               while True:
-                    columnsize = struct.unpack('H', record[pos:pos + 2])[0]
+            while True:
+                columnsize = struct.unpack('H', record[pos:pos + 2])[0]
 
-                    columns += 1
+                columns += 1
 
-                    if (columns + 7) % 8 == 0:
-                         # save the current indicator byte in integer depending
-                         # on current column number for later usage
-                         a = int((columns + 7) / 8) - 1
-                         indi = struct.unpack('B',
-                                              record[a:a+1])[0]
-                         bitpos = (columns + 7) % 8 + 1
+                if (columns + 7) % 8 == 0:
+                    # save the current indicator byte in integer depending
+                    # on current column number for later usage
+                    a = int((columns + 7) / 8) - 1
+                    indi = struct.unpack('B',
+                                     record[a:a+1])[0]
+                    bitpos = (columns + 7) % 8 + 1
 
-                    if columnsize > recordlen - pos - 2:
-                         # columnsize is too big for record. Wrong indicatorpos
-                         columns = -1
-                         break
-                    elif columnsize == recordlen - pos - 2:
-                         # last column in record and reached correct end
-                         # this is the only clean exit from this loop
-                         break
-                    else:
-                         # after columnsize there is still room in record
-                         pos += columnsize + 2
-
-                         # check if column content and indicator express same
-                         if checkindicatorifcolumnisnull (
-                              indi, bitpos) and columnsize == 0:
-                              # ok
-                              pass
-                         elif not checkindicatorifcolumnisnull (
-                              indi, bitpos) and columnsize > 0:
-                              # ok
-                              pass
-                         else:
-                              columns = -1
-                              break
-
-               if columns > 0:
-                    # wow, number of columns found
-                    # still do some checks
-                    if int((columns + 7) / 8) == indicatorpos:
-                         # number of indicator bytes match number of columns
-                         break
-                    else:
-                         # number of indicator bytes do not match number of
-                         # columns .. go to next indicator position
-                         indicatorpos += 1
-               else:
-                    # did not find correct number of columns try next
-                    # indicator
-                    indicatorpos += 1
-
-     elif indicator == 2:
-          # data has no indicator
-          pos = 0
-
-          while pos < recordlen:
-               # still not at the end of the record
-               columnsize = struct.unpack('H', record[pos:pos + 2])[0]
-
-               pos += 2
-
-               if pos + columnsize < recordlen:
-                    # column is as long as expected (last column)
-                    columns += 1
-                    pos += columnsize
-               elif pos + columnsize > recordlen:
-                    # error (columnsize bigger zero but not as big as expected)
+                if columnsize > recordlen - pos - 2:
+                    # columnsize is too big for record. Wrong indicatorpos
                     columns = -1
-                    if source == 0:
-                         logging.warning("File: %s, Error in row %d, "
-                                         "column %d. Found %d bytes "
-                                         "instead of %d!", filename, rownum,
-                                         columns, len(column), columnsize)
                     break
-               else:
-                    # sum of columns equals expected record length
-                    columns += 1
+                elif columnsize == recordlen - pos - 2:
+                    # last column in record and reached correct end
+                    # this is the only clean exit from this loop
                     break
+                else:
+                    # after columnsize there is still room in record
+                    pos += columnsize + 2
 
-     else:
-          # no indicator information given (indicator is unknown)
-          # trying first without indicator
-          columns = numcolumns (filename, record, 2, rownum, 1)
-          
-          if columns < 0:
-               # no result, try with indicator now
-               logging.debug("File: %s, Row %d, no indicator ... "
-                             "trying with indicator", filename, rownum)
-               columns = numcolumns (filename, record, 1, rownum, 1)
+                    # check if column content and indicator express same
+                    if checkindicatorifcolumnisnull (
+                        indi, bitpos) and columnsize == 0:
+                        # ok
+                        pass
+                    elif not checkindicatorifcolumnisnull (
+                        indi, bitpos) and columnsize > 0:
+                        # ok
+                        pass
+                    else:
+                        columns = -1
+                        break
 
-     return (columns)
+            if columns > 0:
+                # wow, number of columns found
+                # still do some checks
+                if int((columns + 7) / 8) == indicatorpos:
+                    # number of indicator bytes match number of columns
+                    break
+                else:
+                    # number of indicator bytes do not match number of
+                    # columns .. go to next indicator position
+                    indicatorpos += 1
+            else:
+                # did not find correct number of columns try next
+                # indicator
+                indicatorpos += 1
+
+    elif indicator == 2:
+        # data has no indicator
+        pos = 0
+
+        while pos < recordlen:
+            # still not at the end of the record
+            columnsize = struct.unpack('H', record[pos:pos + 2])[0]
+
+            pos += 2
+
+            if pos + columnsize < recordlen:
+                # column is as long as expected (last column)
+                columns += 1
+                pos += columnsize
+            elif pos + columnsize > recordlen:
+                # error (columnsize bigger zero but not as big as expected)
+                columns = -1
+                if source == 0:
+                    logging.warning("File: %s, Error in row %d, "
+                                    "column %d. Found %d bytes "
+                                    "instead of %d!", filename, rownum,
+                                    columns, len(column), columnsize)
+                break
+            else:
+                # sum of columns equals expected record length
+                columns += 1
+                break
+
+    else:
+        # no indicator information given (indicator is unknown)
+        # trying first without indicator
+        columns = numcolumns (filename, record, 2, rownum, 1)
+        
+        if columns < 0:
+            # no result, try with indicator now
+            logging.debug("File: %s, Row %d, no indicator ... "
+                          "trying with indicator", filename, rownum)
+            columns = numcolumns (filename, record, 1, rownum, 1)
+
+    return (columns)
