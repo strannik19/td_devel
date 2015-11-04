df070cce179d985d54277837d1c7fc238db9a7e5
Andreas Wenzel <andreas.wenzel@teradata.com>
2015-11-04 15:40:54 +0100

    Enhanced with steps to be able to execute ranges of steps (like starting from (and until) an individual step)

--- ELT/Training_Lab/reset_Training_Lab.sh
+++ ELT/Training_Lab/reset_Training_Lab.sh
@@ -5,64 +5,100 @@
 #   Folders with Training Lab scripts
 #   Existing GCFR installation
 
+# Check if start step specified
+if [ -z "$1" ] 
+then
+   StartStep=1
+else
+   StartStep="$1"
+fi
+
+# Check is end step specified
+if [ -z "$2" ] 
+then
+   EndStep=9999
+else
+   EndStep="$2"
+fi
+
 export mylog="${HOME}/reset_Training_Lab.log"
 
 rm -f ${mylog}
 
-echo -n "Starting preparation input data, logs and scripts ..."
-echo -e "\n################################################################################################## Starting prepare folders" >>${mylog}
-{
-    cd /GCFR_Root
-    rm {logs,logon}/*
-    rm scripts/[A-Z]*
-    rm export_data/archive/*
-    rm export_data/[A-Z]*
-
-    cd source_data
-    mv customer/archive/CUSTOMER_* customer/
-    rm customer/archive/*
-    mv accounts/archive/ACCOUNTS_* accounts/
-    rm accounts/archive/*
-    mv transactions/archive/TRANSACTIONS_* transactions
-    rm transactions/archive/*
-} >>${mylog} 2>&1
-echo " done"
-
-echo -n "Starting reset databases ..."
-echo -e "\n################################################################################################## Starting reset databases" >>${mylog}
-cd ${HOME}/2_GCFR_Standard_Packages
-bteq <Typical_database_clear_script.txt >>${mylog} 2>&1
-RC=$?
-if [ ${RC} -ne 0 ]
+Step=1
+if [ $Step -ge $StartStep ] && [ $Step -le $EndStep ]
 then
-    echo " failed. Please check log file!"
-    echo -e "\n################################################################################################## End reset databases: ${RC}" >>${mylog}
-    exit 1
-fi
-echo " done"
+    echo -n "Step 1: Starting preparation input data, logs and scripts ..."
+    echo -e "\n################################################################################################## Starting prepare folders" >>${mylog}
+    {
+        cd /GCFR_Root
+        rm {logs,logon}/*
+        rm scripts/[A-Z]*
+        rm export_data/archive/*
+        rm export_data/[A-Z]*
 
-echo -n "Starting installation of GCFR ..."
-echo -e "\n################################################################################################## Starting installation GCFR" >>${mylog}
-cd ${HOME}/2_GCFR_Standard_Packages/GCFR_DDL_Code/GCFR_Implementation_Kit
-echo "" | sh runme.sh >> ${mylog} 2>&1
-RC=${PIPESTATUS[1]}
-if [ ${RC} -ne 0 ]
-then
-    echo " failed. Please check log file!"
-    echo -e "\n################################################################################################## End installation GCFR: ${RC}" >>${mylog}
-    exit 2
+        cd source_data
+        mv customer/{archive,loading}/CUSTOMER_* customer
+        rm customer/{archive,loading}/*
+        mv accounts/{archive,loading}/ACCOUNTS_* accounts
+        rm accounts/{archive,loading}/*
+        mv transactions/{archive,loading}/TRANSACTIONS_* transactions/
+        rm transactions/{archive,loading}/*
+        rm -f /opt/teradata/client/15.00/tbuild/{checkpoint,logs}/*
+    } >>${mylog} 2>&1
+    echo " done"
 fi
-echo " done"
 
-echo -n "Creating Training Lab Objects ..."
-echo -e "\n################################################################################################## Starting creating Training Lab" >>${mylog}
-cd ${HOME}/4_Objects_For_Exercises
-bteq <run_all.bteq >> ${mylog} 2>&1
-RC=$?
-if [ ${RC} -ne 0 ]
+
+Step=2
+if [ $Step -ge $StartStep ] && [ $Step -le $EndStep ]
 then
-    echo " failed. Please check log file!"
-    echo -e "\n################################################################################################## End creating Trainig Lab: ${RC}" >>${mylog}
-    exit 3
+    echo -n "Step 2: Starting reset databases ..."
+    echo -e "\n################################################################################################## Starting reset databases" >>${mylog}
+    cd ${HOME}/2_GCFR_Standard_Packages
+    bteq <Typical_database_clear_script.txt >>${mylog} 2>&1
+    RC=$?
+    if [ ${RC} -ne 0 ]
+    then
+        echo " failed. Please check log file!"
+        echo -e "\n################################################################################################## End reset databases: ${RC}" >>${mylog}
+        exit 1
+    fi
+    echo " done"
 fi
-echo " done"
+
+
+Step=3
+if [ $Step -ge $StartStep ] && [ $Step -le $EndStep ]
+then
+    echo -n "Step 3: Starting installation of GCFR ..."
+    echo -e "\n################################################################################################## Starting installation GCFR" >>${mylog}
+    cd ${HOME}/2_GCFR_Standard_Packages/GCFR_DDL_Code/GCFR_Implementation_Kit
+    echo "" | sh runme.sh >> ${mylog} 2>&1
+    RC=${PIPESTATUS[1]}
+    if [ ${RC} -ne 0 ]
+    then
+        echo " failed. Please check log file!"
+        echo -e "\n################################################################################################## End installation GCFR: ${RC}" >>${mylog}
+        exit 2
+    fi
+    echo " done"
+fi
+
+
+Step=4
+if [ $Step -ge $StartStep ] && [ $Step -le $EndStep ]
+then
+    echo -n "Step 4: Creating Training Lab Objects ..."
+    echo -e "\n################################################################################################## Starting creating Training Lab" >>${mylog}
+    cd ${HOME}/4_Objects_For_Exercises
+    bteq <run_all.bteq >> ${mylog} 2>&1
+    RC=$?
+    if [ ${RC} -ne 0 ]
+    then
+        echo " failed. Please check log file!"
+        echo -e "\n################################################################################################## End creating Trainig Lab: ${RC}" >>${mylog}
+        exit 3
+    fi
+    echo " done"
+fi
