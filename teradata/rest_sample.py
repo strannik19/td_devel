d122d37162aaa6fb1b295f4bed4122ed79c83eb7
Andreas Wenzel <andreas.wenzel@teradata.com>
2015-11-11 19:36:04 +0100

    Better formatting. More flexible. Have tried to convert it to python 3.4. Bloody hell. The new string <-> byte construct is causing me headache.

--- teradata/rest_sample.py
+++ teradata/rest_sample.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!python
 
 ##########################################################################
 #    rest_sample.py
@@ -28,23 +28,23 @@ import json
 import urllib2
 import base64
 import zlib
- 
- 
-username = 'sysdba'
-password = 'sysdba'
-teradataDatabaseAlias = 'Teradata15VM'
- 
- 
+
+
+username = 'dbc'
+password = 'dbc'
+teradataDatabaseAlias = 'TMM'
+RestHost = 'tmm'
+
 # HTTP
 # Connect to Teradata via REST
 
-url = 'http://tdevm1500:1080/tdrest/systems/' + teradataDatabaseAlias + '/queries'
+url = 'http://' + RestHost + ':1080/tdrest/systems/' + teradataDatabaseAlias + '/queries'
  
- 
+
 # HTTPS 
 #url = 'https://tdevm1500:1443/tdrest/systems/' + teradataDatabaseAlias + '/queries'
  
- 
+
 # Setup required HTTP headers
 headers={}
 headers['Content-Type'] = 'application/json'
@@ -61,26 +61,26 @@ queryBands = {}
 queryBands['applicationName'] = 'MyApp'
 queryBands['version'] = '1.0'
  
- 
+
 # Set request fields, including SQL.
 data = {}
 data['query'] = 'SELECT * FROM DBC.DBCInfo'
 data['queryBands'] = queryBands
 data['format'] = 'array'
  
- 
+
 # Build request.
 request = urllib2.Request(url, json.dumps(data), headers)
  
  
 #Submit request
 try:
-  response = urllib2.urlopen(request);
-  # Check if result have been compressed.
-  if response.info().get('Content-Encoding') == 'gzip':  
-    response = zlib.decompress(response.read(), 16+zlib.MAX_WBITS)    
-  else:
-    response = response.read();
+    response = urllib2.urlopen(request);
+    # Check if result have been compressed.
+    if response.info().get('Content-Encoding') == 'gzip':  
+        response = zlib.decompress(response.read(), 16 + zlib.MAX_WBITS)    
+    else:
+        response = response.read();
 except urllib2.HTTPError, e:
     print 'HTTPError = ' + str(e.code)
     response = e.read();
