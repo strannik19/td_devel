18c032ba5bd999d2c814c74757c394cede917961
Andreas Wenzel <andreas.wenzel@teradata.com>
2016-02-05 12:10:12 +0100

    Changed indenting from tabs to spaces. Also changed download to first regularly try to download. If this fails, try to download with option "--no-check-certificate" to ignore certificate check. Unfortunately, this is required for some sources.

--- ELT/Install_TD_Developers_Package_SLES11SP1/download.sh
+++ ELT/Install_TD_Developers_Package_SLES11SP1/download.sh
@@ -26,41 +26,44 @@
 ERRORPACKAGES=""
 
 for DL in \
-	http://ftp.fau.de/apache/apr/apr-1.5.2.tar.bz2 \
-	http://ftp.fau.de/apache/apr/apr-util-1.5.4.tar.bz2 \
-	http://curl.haxx.se/download/curl-7.40.0.tar.bz2 \
-	https://www.kernel.org/pub/software/scm/git/git-2.7.0.tar.gz \
-	http://openssl.org/source/openssl-1.0.2f.tar.gz \
-	http://prdownloads.sourceforge.net/scons/scons-local-2.3.4.tar.gz \
-	http://serf.googlecode.com/svn/src_releases/serf-1.3.8.tar.bz2 \
-	http://www.sqlite.org/2015/sqlite-amalgamation-3080801.zip \
-	http://archive.apache.org/dist/subversion/subversion-1.9.3.tar.bz2
+    http://ftp.fau.de/apache/apr/apr-1.5.2.tar.bz2 \
+    http://ftp.fau.de/apache/apr/apr-util-1.5.4.tar.bz2 \
+    http://curl.haxx.se/download/curl-7.40.0.tar.bz2 \
+    https://www.kernel.org/pub/software/scm/git/git-2.7.0.tar.gz \
+    http://openssl.org/source/openssl-1.0.2f.tar.gz \
+    http://prdownloads.sourceforge.net/scons/scons-local-2.3.4.tar.gz \
+    http://serf.googlecode.com/svn/src_releases/serf-1.3.8.tar.bz2 \
+    http://www.sqlite.org/2015/sqlite-amalgamation-3080801.zip \
+    http://archive.apache.org/dist/subversion/subversion-1.9.3.tar.bz2
 do
-	if [ "${DL#https://cpan.metacpan.org}" = "${DL}" ]
-	then
-		wget "${DL}" -O "${DL##*/}"
-	else
-		wget --no-check-certificate "${DL}" -O "${DL##*/}"
-	fi
+    wget "${DL}" -O "${DL##*/}"
+    RC=$?
+    if [ ${RC} -eq 1 ]
+    then
+        # If download fails, try without checking the certificate
+        # Unfortunately, this is required for some sources
+        wget --no-check-certificate "${DL}" -O "${DL##*/}"
+        RC=$?
+    fi
 
-	if [ $? -ne 0 ]
-	then
-		echo "Error downloading package $DL!"
-		if [ -z "${ERRORPACKAGES}" ]
-		then
-			ERRORPACKAGES="${DL##*/}"
-		else
-			ERRORPACKAGES="${ERRORPACKAGES} ${DL##*/}"
-		fi
-	fi
+    if [ $RC -ne 0 ]
+    then
+        echo "Error downloading package $DL!"
+        if [ -z "${ERRORPACKAGES}" ]
+        then
+            ERRORPACKAGES="${DL##*/}"
+        else
+            ERRORPACKAGES="${ERRORPACKAGES} ${DL##*/}"
+        fi
+    fi
 
 done
 
 if [ -z "${ERRORPACKAGES}" ]
 then
-	echo "Successfully downloaded all packages!"
-	exit 0
+    echo "Successfully downloaded all packages!"
+    exit 0
 else
-	echo "Error in downloading package(s): ${ERRORPACKAGES}"
-	exit 1
+    echo "Error in downloading package(s): ${ERRORPACKAGES}"
+    exit 1
 fi
